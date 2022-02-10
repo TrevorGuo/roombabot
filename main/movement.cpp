0610732a7b6d57f75a4a6eed24a164c76c58aab7
TrevorGuo <trevorguo@gmail.com>
2022-02-09 17:02:29 -0800

    Adjustable motor speed, and solved problem with delayed ultrasonic reaction(hardware issue not software)

--- main/movement.cpp
+++ main/movement.cpp
@@ -1,7 +1,6 @@
-// #include <Wire.h>
-
 #include "variables.h"
-#include "sensors.ino"
+#include "sensors.h"
+#include "movement.h"
 
 /* const int MPU = 0x68; // MPU6050 I2C address
 
@@ -20,24 +19,20 @@ float getYaw() {    // what file should this be in lol
 
 void forward() {  // move forward until an object is detected within a certain threshold distance
     readUltrasonicSensors();
-
-    if (frontDist >= 15 && leftDist >= 15 && rightDist >= 15) {   // arbitrary for now
-        digitalWrite(ENA, HIGH);
+    if (frontDist >= 15 || leftDist >= 15 || rightDist >= 15) {   // arbitrary for now
+        analogWrite(ENA, motorSpeed);
         digitalWrite(IN1, HIGH);
         digitalWrite(IN2, LOW);
-        digitalWrite(ENB, HIGH);
+        analogWrite(ENB, motorSpeed);
         digitalWrite(IN3, HIGH);
         digitalWrite(IN4, LOW);
     }
-
-    while (frontDist >= 15 && leftDist >= 15 && rightDist >= 15) {
+    while (frontDist >= 15 || leftDist >= 15 || rightDist >= 15) {
         readUltrasonicSensors();
     }
-
+    
     analogWrite(ENA, LOW);
     analogWrite(ENB, LOW);
-
-    }
 }
 
 void rotate(float degree) {
---
+++ main/movement.h
@@ -0,0 +1,9 @@
+#ifndef MOVEMENT_H
+#define MOVEMENT_H
+
+float getYaw();
+void forward();
+void rotate(float degree);
+void aroundObstacle();
+
+#endif // MOVEMENT_H
